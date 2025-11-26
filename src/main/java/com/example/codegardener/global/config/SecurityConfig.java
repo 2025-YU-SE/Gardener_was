fe376ed3fbd33ba53017d448ca6278c97747f1d1
PARKJEHWAN <gfds52@naver.com>
2025-11-26 17:14:03 +0900

    signup/login bug fix

--- src/main/java/com/example/codegardener/global/config/SecurityConfig.java
+++ src/main/java/com/example/codegardener/global/config/SecurityConfig.java
@@ -89,7 +89,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         // 필요 시 프론트 서버 주소로 변경
-        config.setAllowedOrigins(List.of("http://localhost:3000", "*"));
+        config.setAllowedOriginPatterns(List.of("http://localhost:3000", "*"));
 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
