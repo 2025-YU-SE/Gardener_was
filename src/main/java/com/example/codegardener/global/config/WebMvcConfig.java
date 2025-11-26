ee9fa5a98192b3b77341a1b0b63765ab9711b87a
yimeunjin <yesim2j@yu.ac.kr>
2025-11-26 17:02:17 +0900

    uploadPath fix

--- src/main/java/com/example/codegardener/global/config/WebMvcConfig.java
+++ src/main/java/com/example/codegardener/global/config/WebMvcConfig.java
@@ -3,12 +3,13 @@ package com.example.codegardener.global.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import java.nio.file.Paths;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
     // 업로드된 파일이 저장될 로컬 경로: /uploads
-    private final String uploadPath = "file:///" + System.getProperty("user.dir") + "/uploads/";
+    private final String uploadPath = Paths.get(System.getProperty("user.dir"), "uploads").toUri().toString();
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
