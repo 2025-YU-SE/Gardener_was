193574ca4b353fc60fd0717e8c30442a8c9bd80b
yimeunjin <yesim2j@yu.ac.kr>
2025-11-26 16:50:57 +0900

    FeedbackCommentDto 수정

--- src/main/java/com/example/codegardener/feedback/dto/FeedbackCommentDto.java
+++ src/main/java/com/example/codegardener/feedback/dto/FeedbackCommentDto.java
@@ -1,6 +1,7 @@
 package com.example.codegardener.feedback.dto;
 
 import com.example.codegardener.feedback.domain.FeedbackComment;
+import com.example.codegardener.user.domain.UserProfile;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -13,13 +14,23 @@ import java.time.LocalDateTime;
 public class FeedbackCommentDto {
     private Long commentId;
     private Long userId;
+    private String userName;
+    private String userPicture;
     private String content;
     private LocalDateTime createdAt;
 
     public static FeedbackCommentDto fromEntity(FeedbackComment comment) {
+        String userPicture = null;
+        UserProfile profile = comment.getUser().getUserProfile();
+        if (profile != null) {
+            userPicture = profile.getUserPicture();
+        }
+
         return FeedbackCommentDto.builder()
                 .commentId(comment.getCommentId())
                 .userId(comment.getUser().getUserId())
+                .userName(comment.getUser().getUserName())
+                .userPicture(userPicture)
                 .content(comment.getContent())
                 .createdAt(comment.getCreatedAt())
                 .build();
